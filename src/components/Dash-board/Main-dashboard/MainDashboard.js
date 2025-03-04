2d4a2bcafd85830071a20d1d8cf96836cd6f79a6
kbamnote3 <kunalbamnote7@gmail.com>
2025-03-04 10:39:32 +0530

    k2

--- src/components/Dash-board/Main-dashboard/MainDashboard.js
+++ src/components/Dash-board/Main-dashboard/MainDashboard.js
@@ -1,48 +1,35 @@
-import React, { useState } from 'react';
-import Sidebar from '../../Common-components/Sidebar/Sidebar';
-import DashCategories from './Dashboard-components/DashCategories';
-import DashLocation from './Dashboard-components/DashLocation';
-import DashLegalPolicy from './Dashboard-components/DashLegalPolicy';
-import DashReport from './Dashboard-components/DashReport';
-import DashRating from './Dashboard-components/DashRating';
-import Navbar from '../../Common-components/Navbar/Navbar';
+import React from "react";
+import Sidebar from "../../Common-components/Sidebar/Sidebar";
+import DashCategories from "./Dashboard-components/DashCategories";
+import DashLocation from "./Dashboard-components/DashLocation";
+import DashLegalPolicy from "./Dashboard-components/DashLegalPolicy";
+import DashReport from "./Dashboard-components/DashReport";
+import DashRating from "./Dashboard-components/DashRating";
+import Navbar from "../../Common-components/Navbar/Navbar";
+
 
 const MainDashboard = () => {
-  const [showUserMenu, setShowUserMenu] = useState(false);
-
   return (
-    <div className="flex h-screen bg-[#F4F7FF94]">
-      <div className="w-60 bg-black text-white">
+    <div className="flex flex-col md:flex-row h-screen bg-[#F4F7FF94]">
+      <div className="md:block md:static fixed z-20 top-0 left-0 h-full bg-white transition-all duration-900">
         <Sidebar />
       </div>
-
-      <div className="flex-1 overflow-auto p-8">
-        <div className="flex justify-between items-center p-4 border-b">
-          <h1 className="text-[32px] font-bold ml-10">Dashboard</h1>
-
+      <div className="flex-1 flex flex-col overflow-hidden bg-[#F4F7FF94] transition-all duration-300">
+        <div className="flex mt-0 md:mt-8 justify-between items-center border-b-2 px-4 md:px-8 py-4">
+          <h1 className="text-xl sm:text-2xl lg:text-3xl 2xl:text-4xl font-bold ml-9 sm:ml-0">
+            Dashboard
+          </h1>
           <Navbar />
         </div>
-
-        <div className="p-8">
-          <div className="flex flex-col mb-8 md:flex-row justify-between items-center">
-            <div className="">
-              <DashCategories />
-            </div>
-            <div className="">
-              <DashLocation />
-            </div>
+        <div className="p-4 lg:p-8 flex-1 overflow-auto">
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6">
+            <DashCategories className="w-full" />
+            <DashLocation className="w-full" />
           </div>
-
-          <div className="flex flex-col md:flex-row justify-between items-center">
-            <div >
-              <DashLegalPolicy />
-            </div>
-            <div>
-              <DashReport />
-            </div>
-            <div >
-              <DashRating />
-            </div>
+          <div className="kunal grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3  gap-6 mt-6">
+            <DashLegalPolicy className="w-full" />
+            <DashReport className="w-full" />
+            <DashRating className="w-full" />
           </div>
         </div>
       </div>
